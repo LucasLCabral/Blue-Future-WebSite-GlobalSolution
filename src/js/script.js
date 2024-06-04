65bba167215911bd3f4414fde5e6b770ae33e194
weslley <weslleycardoso@gmail.com>
2024-06-04 17:38:20 -0300

    Adicionando a sessao Login

--- src/js/script.js
+++ src/js/script.js
@@ -45,3 +45,31 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+// LOGIN
+
+const form = document.querySelector("#form");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
+
+form.addEventListener("submit", (event) =>{
+    event.preventDefault();
+
+    //Verificar se o email está preenchido 
+    if(emailInput.value === ""){
+        alert("Email vazio. Por favor preencha o seu email.");
+        return;
+    }
+
+    //Verificar se a senha está preenchida
+    if(passwordInput.value === ""){
+        alert("Senha vazia. Por favor coloque sua senha.");
+        return;
+    }
+
+    // Selecionar a seção "donate"
+    const donateSection = document.querySelector(".donate");
+
+    // Rolagem suave até a seção "donate"
+    donateSection.scrollIntoView({ behavior: 'smooth' });
+});
